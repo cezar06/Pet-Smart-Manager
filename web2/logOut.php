8d373d0c0667f1a2f6122b43dc09dec43bff211a
cezar06 <50807082+cezar06@users.noreply.github.com>
2022-06-18 23:22:00 +0300

    Delete web2 directory

--- web2/logOut.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-        $_SESSION['logged_in_user'] = '0';
-        $_SESSION['login_user'] = '0';
-        header ("location: index.php");
-        exit;
-    //require 'view.php';
-?>
